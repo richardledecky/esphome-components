e29866d340e1405844f4ace4329d9ee8d35b6c0b
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:29:25 +0200

    Delete component/rc_switch_component directory

--- component/rc_switch_component/rc_switch_component.cpp
+++
@@ -1,9 +0,0 @@
-#include "rc_switch_component.h"
-
-namespace esphome {
-namespace rc_switch_component {
-
-// nothing needed here yet
-
-}  // namespace rc_switch_component
-}  // namespace esphome
