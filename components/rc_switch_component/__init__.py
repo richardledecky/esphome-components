1f186cca8e001316903b3bd26c599dc7379d9a9a
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:33:52 +0200

    Update __init__.py

--- components/rc_switch_component/__init__.py
+++ components/rc_switch_component/__init__.py
@@ -15,14 +15,14 @@ CONFIG_SCHEMA = cv.Schema(
     }
 )
 
-
 @cg.register_action("rc_switch_component.send", SendRCSwitchAction, CONFIG_SCHEMA)
 async def rc_switch_send_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     cg.add(var.set_code(config[CONF_CODE]))
     cg.add(var.set_gpio(config[CONF_GPIO]))
-    # pridáme C++ súbory a knižnicu
+
     cg.add_library("rc-switch", None)
     cg.add_source_file("rc_switch_component.cpp")
     cg.add_source_file("rc_switch_component.h")
+
     return var
