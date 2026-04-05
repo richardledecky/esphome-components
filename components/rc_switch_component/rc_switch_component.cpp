524778341e326045b4e08fe628ce1d240e86b74b
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:46:15 +0200

    Update rc_switch_component.cpp

--- components/rc_switch_component/rc_switch_component.cpp
+++ components/rc_switch_component/rc_switch_component.cpp
@@ -1,9 +1,3 @@
 #include "rc_switch_component.h"
 
-namespace rc_switch_component {
-
-// ak je potrebné, sem môžeš dať implementácie funkcií
-// napríklad pre pin, debounce a pod.
-// momentálne všetko je inline v hlavičke, takže CPP môže zostať takto
-
-}  // namespace rc_switch_component
+// momentálne nič extra, všetko je inline v .h
