ceb2c4168213e3fd739bd055e07e9c3e96334ed0
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 19:43:55 +0200

    Update rc_switch_component.cpp

--- components/rc_switch_component/rc_switch_component.cpp
+++ components/rc_switch_component/rc_switch_component.cpp
@@ -1,9 +1,9 @@
 #include "rc_switch_component.h"
 
-namespace esphome {
 namespace rc_switch_component {
 
-// nothing needed here yet
+// ak je potrebné, sem môžeš dať implementácie funkcií
+// napríklad pre pin, debounce a pod.
+// momentálne všetko je inline v hlavičke, takže CPP môže zostať takto
 
 }  // namespace rc_switch_component
-}  // namespace esphome
