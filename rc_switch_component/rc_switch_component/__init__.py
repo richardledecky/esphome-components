82466a5311dec91e8a1fb0fa7ded561847438745
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-05 18:56:20 +0200

    Update __init__.py

--- rc_switch_component/rc_switch_component/__init__.py
+++ rc_switch_component/rc_switch_component/__init__.py
@@ -1,10 +1,11 @@
 import esphome.codegen as cg
+import esphome.config_validation as cv
+
+CODEOWNERS = ["@richardledecky"]
 
 rc_switch_ns = cg.esphome_ns.namespace("rc_switch_component")
 
+# Registrácia C++ súborov
 cg.add_library("rc-switch", None)
-
-cg.add_global(cg.RawExpression("using namespace rc_switch_component;"))
-
 cg.add_source_file("rc_switch_component.cpp")
 cg.add_source_file("rc_switch_component.h")
