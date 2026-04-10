086cee1f8744a2b42c0457980081ddebebee644e
richardledecky <81380133+richardledecky@users.noreply.github.com>
2026-04-10 09:48:03 +0200

    Update rc_switch_component.h

--- components/rc_switch_component/rc_switch_component.h
+++ components/rc_switch_component/rc_switch_component.h
@@ -1,34 +1,52 @@
 #pragma once
 
-#include "esphome/core/automation.h"
 #include "esphome/core/component.h"
+#include "esphome/core/automation.h"
 #include <RCSwitch.h>
 
 namespace esphome {
 namespace rc_switch_component {
 
-template<typename... Ts>
-class SendRCSwitchAction : public Action<Ts...> {
+class RCSwitchComponent : public Component {
  public:
-  void set_code(uint32_t code) { code_ = code; }
   void set_gpio(int gpio) { gpio_ = gpio; }
 
-  void play(const Ts &...x) override {
-    RCSwitch sw;
+  void setup() override {
+    sw_.enableTransmit(gpio_);
+    sw_.setProtocol(1);
+    sw_.setPulseLength(350);
+    sw_.setRepeatTransmit(10);
+  }
 
-    sw.enableTransmit(gpio_);
-
-    // 🔥 dôležité nastavenia
-    sw.setProtocol(1);        // podľa tvojho logu
-    sw.setPulseLength(350);   // podľa tvojho logu
-    sw.setRepeatTransmit(8);  // 🔥 spoľahlivosť
-
-    sw.send(code_, 24);
+  void send(uint32_t code, uint8_t length) {
+    sw_.send(code, length);
   }
 
  protected:
+  RCSwitch sw_;
+  int gpio_{23};
+};
+
+// ================= ACTION =================
+
+template<typename... Ts>
+class SendRCSwitchAction : public Action<Ts...> {
+ public:
+  void set_parent(RCSwitchComponent *parent) { parent_ = parent; }
+  void set_code(uint32_t code) { code_ = code; }
+  void set_gpio(int gpio) { gpio_ = gpio; }
+
+  void play(Ts... x) override {
+    if (parent_ == nullptr)
+      return;
+
+    parent_->send(code_, 24);
+  }
+
+ protected:
+  RCSwitchComponent *parent_{nullptr};
   uint32_t code_{0};
-  int gpio_{4};   // ← radšej default správny pin
+  int gpio_{23};
 };
 
 }  // namespace rc_switch_component
